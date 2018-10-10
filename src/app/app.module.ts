2931d38e40863dc78686c24c6e65177b489bd280
Gabriel <bucalongabriell@gmail.com>
2018-10-10 14:40:35 -0300

    UPDATE - Criação do componente dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { LoginComponent } from './auth/login/login.component';
 import { LegalAdviceComponent } from './auth/legal-advice/legal-advice.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +21,7 @@ import { LegalAdviceComponent } from './auth/legal-advice/legal-advice.component
     SidebarComponent,
     LoginComponent,
     LegalAdviceComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +30,13 @@ import { LegalAdviceComponent } from './auth/legal-advice/legal-advice.component
     MatButtonModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
